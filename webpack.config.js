9918570ddb80887eb91260ac32fd8592c662160a
Filip Jerga <jerga99@gmail.com>
2016-05-29 10:34:59 +0200

    Run Weback on Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -14,7 +16,12 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
-    })
+    }),
+      new webpack.optimize.UglifyJsPlugin({
+          compressor: {
+              warnings: false
+          }
+      })
   ],
   output: {
     path: __dirname,
@@ -59,5 +66,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
