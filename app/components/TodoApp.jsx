91de23a738db4f4c5606cdf81860fa307770dc8c
Filip Jerga <jerga99@gmail.com>
2016-06-05 19:14:49 +0200

    Header added

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Redux from 'react-redux';
 
+import TodoHeader from 'TodoHeader';
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
@@ -18,6 +19,12 @@ export var TodoApp = React.createClass({
   render() {
     return (
       <div>
+          <TodoHeader />
+          <div className="contain-to-grid sticky">
+              <nav className="top-bar" data-topbar role="navigation" data-options="sticky_on: large">
+                  ...
+              </nav>
+          </div>
           <div className="page-actions">
               <a href="#" onClick={this.onLogout}>Logout</a>
           </div>
